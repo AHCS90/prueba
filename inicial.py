270f1fc451dff2cefa42de0ca2a74cacfc4f00c8
AHCS90 <35605685+AHCS90@users.noreply.github.com>
2018-02-05 16:44:30 +0100

    Fichero inicial de prueba

---
+++ inicial.py
@@ -0,0 +1,18 @@
+2+2
+print (2+2)
+print (17/3)
+prefix = 'Py'
+word = prefix + 'thon'
+print word [0:6]
+a = 'Ana'
+b = 'Nerd'
+c = 'Python'
+d= a + ' es una ' + b + ' porque sabe '+c
+print d 
+squares = []
+squares = ['cat', 4, 3.2]
+print squares [-1]
+squares = squares + [81, 'dog']
+squares.remove(3.2)
+squares.append('horse')
+print squares
